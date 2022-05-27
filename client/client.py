8ac11b72eb6cb44fe5271c7a7770d874db39b3f3
VinayakaKS <vinayakaksgupta@gmail.com>
2022-05-27 23:59:39 +0530

    Made changes

--- client/client.py
+++ client/client.py
@@ -4,10 +4,9 @@ import json
 
 test_image = open('Image.jpg','rb')
 files = {'image': test_image}
-BASE = "http://127.0.0.1:5000/"
+BASE = "http://192.168.122.1:5000/"
 
 with open("Image.jpg", "rb") as image2string:
     converted_string = base64.b64encode(image2string.read())
-print(converted_string)
 response = requests.post(BASE, json = {'image_string':converted_string})
 print(response.text)
